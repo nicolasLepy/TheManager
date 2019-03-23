d42a44f0228b9a41f50ef4264b6bc7139f30548f
nicoapps <33573107+nicoapps@users.noreply.github.com>
2019-03-23 11:33:42 +0100

    Ajoutez des fichiers projet.

---
+++ TheManager/Joueur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TheManager
+{
+    public class Joueur
+    {
+        private string _nom;
+        private string _prenom;
+        private DateTime _naissance;
+        private int _niveau;
+        private int _potentiel;
+    }
+}
