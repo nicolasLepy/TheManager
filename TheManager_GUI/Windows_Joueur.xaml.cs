8b0026606392f974dd73694caa934e9b6ee40db3
Nicolas Lépy <33573107+nicolasLepy@users.noreply.github.com>
2020-06-26 12:36:13 +0200

    Removed useless using

--- TheManager_GUI/Windows_Joueur.xaml.cs
+++ TheManager_GUI/Windows_Joueur.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using TheManager;
 
 namespace TheManager_GUI
